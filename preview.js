5160d9b621e07355015cabc0b407f13846ccaf5f
michaelsnook <michael.snook@gmail.com>
2014-02-28 13:28:16 -0500

    minor change

--- preview.js
+++ preview.js
@@ -6,10 +6,8 @@ $(document).ready(function() {
   css.setAttribute('href', 'preview.css')
   document.getElementsByTagName('head')[0].appendChild(css)
 
-  var bar = $('<div id="preview-bar">');
-  
-  bar.append('<ul><li><a href="househtml.html">house</a></li><li><a href="senatehtml.html">senate</a></li></ul>');
-  bar.find('li');
+  var bar = $('<div id="preview-bar"><ul><li><a href="househtml.html">house</a></li><li><a href="senatehtml.html">senate</a></li></ul></div>');
+
   
   $('body').append(bar);
 });
