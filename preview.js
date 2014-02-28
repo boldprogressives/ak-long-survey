949d65cc78bd6c3bdb4ef4f376ae1fe3da506d37
michaelsnook <michael.snook@gmail.com>
2014-02-28 13:25:04 -0500

    adding preview nav for my own convenience

---
+++ preview.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+  var css=document.createElement('link')
+  css.setAttribute('rel', 'stylesheet')
+  css.setAttribute('type', 'text/css')
+  css.setAttribute('href', 'preview.css')
+  document.getElementsByTagName('head')[0].appendChild(css)
+
+  var bar = $('<div id="preview-bar">');
+  
+  bar.append('<ul><li><a href="househtml.html">house</a></li><li><a href="senatehtml.html">senate</a></li></ul>');
+  bar.find('li');
+  
+  $('body').append(bar);
+});
+
+
