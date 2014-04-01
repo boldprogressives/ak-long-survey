7ccbb4a91971354196476f81becb3c35edc37ffb
Laura <laura@boldprogressives.org>
2014-04-01 15:40:44 -0500

    remove extra debug lines

--- print.js
+++ print.js
@@ -9,7 +9,6 @@ function update_all(){
     $('textarea, input[type=text]').each(function(){
       var input_name = $(this).attr('name');
       var answer = $(this).val();
-      console.log($('#print-'+input_name).length);
       if($('#print-'+input_name).length == 0){
       $('#print').find("[name="+input_name+"]").after('<span id="print-'+input_name+'">Your response: '+answer+'</span>');
       }
